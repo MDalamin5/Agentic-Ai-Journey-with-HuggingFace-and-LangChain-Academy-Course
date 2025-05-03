cf05d9cb255ec9b8da951e1d6b4766a7b3fa8cc3
MDalamin5 <mdal.amin5@northsouth.edu>
2025-05-03 20:35:46 +0600

    implement the simple agent and explore LnagGraph interface to experiment

---
+++ LangGraph-Agent-Course/Module-1-Introduction/studio/router.py
@@ -0,0 +1,43 @@
+from langchain_openai import ChatOpenAI
+from langgraph.graph import MessagesState
+from langgraph.graph import StateGraph, START, END
+from langgraph.prebuilt import ToolNode, tools_condition
+from langchain_groq import ChatGroq
+import os
+from dotenv import load_dotenv
+load_dotenv()
+gorq_api_key = os.getenv("GROQ_API_KEY")
+# Tool
+def multiply(a: int, b: int) -> int:
+    """Multiplies a and b.
+
+    Args:
+        a: first int
+        b: second int
+    """
+    return a * b
+
+# LLM with bound tool
+# llm = ChatOpenAI(model="gpt-4o")
+llm = ChatGroq(model_name = "meta-llama/llama-4-scout-17b-16e-instruct", gorq_api_key=gorq_api_key)
+llm_with_tools = llm.bind_tools([multiply])
+
+# Node
+def tool_calling_llm(state: MessagesState):
+    return {"messages": [llm_with_tools.invoke(state["messages"])]}
+
+# Build graph
+builder = StateGraph(MessagesState)
+builder.add_node("tool_calling_llm", tool_calling_llm)
+builder.add_node("tools", ToolNode([multiply]))
+builder.add_edge(START, "tool_calling_llm")
+builder.add_conditional_edges(
+    "tool_calling_llm",
+    # If the latest message (result) from assistant is a tool call -> tools_condition routes to tools
+    # If the latest message (result) from assistant is a not a tool call -> tools_condition routes to END
+    tools_condition,
+)
+builder.add_edge("tools", END)
+
+# Compile graph
+graph = builder.compile()
